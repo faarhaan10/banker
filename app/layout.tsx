4fa61c9964b6f5e447771ec8bde52990629438a0
faarhaan10 <faarhaan10@gmail.com>
2024-09-05 20:12:31 +0600

    temp codes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from "next";
-import { Inter ,IBM_Plex_Serif} from "next/font/google";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] ,variable:'--font-inter'});
-const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"] ,variable:'--font-ibm-plex-serif',weight:['400','700']});
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"], variable: '--font-ibm-plex-serif', weight: ['400', '700'] });
 
 export const metadata: Metadata = {
   title: "Banker",
   description: "Banker is a banking app for everyone!",
   icons: {
-    icon:'/icons/logo.svg'
-  } 
+    icon: '/icons/logo.svg'
+  }
 };
 
 export default function RootLayout({
